d88839e6c61ee2ab07844f04854c03f46caa4302
Mathael76 <codypronnier@proton.me>
2023-08-05 20:31:05 +0200

    Version 0.1

---
+++ Personnage.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiVersusGame
+{
+    public class Personnage
+    {
+        public string Nom { get; private set; }
+        public int PointsDeVie { get; private set; }
+        public int Force { get; private set; }
+        public int Defense { get; private set; }
+
+        public Personnage(string nom, int pointsDeVie, int force, int defense)
+        {
+            Nom = nom;
+            PointsDeVie = pointsDeVie;
+            Force = force;
+            Defense = defense;
+        }
+
+        public void Attaquer(Personnage cible)
+        {
+            // Calculer les dégâts infligés en fonction de la force et de la défense
+            int degats = Math.Max(0, Force - cible.Defense);
+
+            Console.WriteLine($"{Nom} attaque {cible.Nom} et inflige {degats} dégâts !");
+
+            // Appliquer les dégâts à la cible
+            cible.SubirDegats(degats);
+        }
+
+        public void Afk()
+        {
+
+            Console.WriteLine($"{Nom} passe son tour (le gros gland) !");
+
+        }
+
+        public void SubirDegats(int montant)
+        {
+            PointsDeVie -= montant;
+
+            if (PointsDeVie <= 0)
+            {
+                PointsDeVie = 0;
+                Console.WriteLine($"{Nom} a été vaincu !");
+            }
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MultiVersusGame;
+using MultiVersusGame.utils;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        Personnage mysthen = new Personnage("Mysthen", 10, 10, 10);
+        Personnage ysarion = new Personnage("Ysarion", 15, 15, 5);
+
+
+        string[,] options = { { "Attaquer" },
+                              { "Afk" } };
+
+        GridNavigation gridNav = new GridNavigation(options);
+
+        mysthen.Attaquer(ysarion);
+        ysarion.Attaquer(mysthen);
+        mysthen.Afk();
+        ysarion.Attaquer(mysthen);
+    }
+}
